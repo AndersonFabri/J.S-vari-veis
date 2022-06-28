d04eb6c7d6d1a3062b956eeca89688e8d3400198
AndersonFabri <107128523+AndersonFabri@users.noreply.github.com>
2022-06-28 11:28:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = "Anderson"
+let sobrenome = "Rodrigues"
+let anoNascimento = 2001;
+let cidade = "Iretama"
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento;
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
+
